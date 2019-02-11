ce1c40577eccade843caa5f39a0e93133a1f0301
Carlos O Carvalho <cjc@modalnetowrks.com>
2019-02-11 07:14:40 -0200

    correction path config

--- src/Providers/LaravelServiceProvider.php
+++ src/Providers/LaravelServiceProvider.php
@@ -22,10 +22,10 @@ class LaravelServiceProvider extends ServiceProvider
 
 
     public function boot(){
-         $this->mergeConfigFrom(__DIR__ .'../config/metasearch', 'metasearch');
+         $this->mergeConfigFrom(__DIR__ .'/../config/metasearch', 'metasearch');
 
          $this->publishes([
-             __DIR__ .'/config/' => config_path()
+             __DIR__ .'/../config/' => config_path()
          ], 'metasearch.config');
     }
 
